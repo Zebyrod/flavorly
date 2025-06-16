e930532680f1424ef7bc3149759b9fd72a2f7283
Zebastian Rodriguez <Zebo5914@gmail.com>
2025-06-15 22:28:00 -0400

    MVP and signin/up/out added

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
 
-    # Recipe URLs
     # Recipe URLs
 path('recipes/', views.recipe_index, name='recipe-index'),
 path('recipes/create/', views.recipe_create, name='recipe-create'),
@@ -19,4 +18,7 @@ path('recipes/<int:recipe_id>/ingredients/', views.ingredient_list, name='ingred
 path('recipes/<int:recipe_id>/ingredients/add/', views.ingredient_create, name='ingredient-create'),
 path('recipes/<int:recipe_id>/ingredients/<int:ingredient_id>/delete/', views.ingredient_delete, name='ingredient-delete'),
 
+# Log In URLS
+path('accounts/', include('django.contrib.auth.urls')),
+path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Recipe, Ingredient
 from .forms import RecipeForm, IngredientForm
 
@@ -111,3 +113,14 @@ def ingredient_delete(request, recipe_id, ingredient_id):
         'ingredient': ingredient,
         'recipe': recipe
     })
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in immediately after signup
+            return redirect('home')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
