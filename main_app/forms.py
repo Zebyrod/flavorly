9bfe29bc0923fc52d0ef57afbe36cff8f8a70b18
Zebastian Rodriguez <Zebo5914@gmail.com>
2025-06-15 20:21:37 -0400

    waiving white flag/ refactor to be simpler

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from .models import Recipe, Ingredient
-from django.forms.models import inlineformset_factory
 
-# I went back in here and refactored my forms in order to implement the bootstrap styling without having to use a custom filter
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
@@ -22,12 +20,3 @@ class IngredientForm(forms.ModelForm):
             'unit': forms.Select(attrs={'class': 'form-select'}),
             'name': forms.TextInput(attrs={'class': 'form-control'}),
         }
-
-IngredientFormSet = inlineformset_factory(
-    Recipe,
-    Ingredient,
-    form=IngredientForm,
-    fields=['amount', 'unit', 'name'],
-    extra=1, # This will populate one blank form to start 
-    can_delete=True # This quick line allows me to implement deletion of the ingredient within the form. This will be a checkbox where the user can click then save and it will delete. Will revisit this later to add in another view potentially where the user will get a are you sure page etc.  
-)
--- main_app/views.py
+++ main_app/views.py
@@ -1,102 +1,125 @@
-from django.shortcuts import render, redirect 
-from .models import Recipe, Ingredient
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
-
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.views import LoginView
-
-from .forms import RecipeForm, IngredientFormSet, IngredientForm
-from django.forms import formset_factory
+from django.views.generic import CreateView, UpdateView, DetailView, DeleteView
+from .models import Recipe, Ingredient
+from .forms import RecipeForm, IngredientForm
 
 # Create your views here.
 
-# Define the home view function
+# Home and About Views
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
+# Recipe Views
+
 @login_required
 def recipe_index(request):
     recipes = Recipe.objects.filter(user=request.user)
-    # Render the recipes/index.html with the data
-    return render(request, 'recipes/index.html', {'recipes': recipes })
+    return render(request, 'recipes/index.html', {'recipes': recipes})
 
 def recipe_detail(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     ingredients = Ingredient.objects.filter(recipe=recipe)
-
-    # instantiate the ingredient form to be rendered in the recipe template
-    IngredientFormSet = formset_factory(IngredientForm, extra=1)
+    ingredient_form = IngredientForm()
     return render(request, 'recipes/details.html', {
         'recipe': recipe,
-        'ingredients': ingredients 
+        'ingredients': ingredients,
+        'ingredient_form': ingredient_form,
     })
 
-# Since I ended up using a formset I had to vastly refactor my views.py file from what was used in class to achieve the functionality I wanted. The goal was to have a separate model for ingredients and allow the user to add as many ingredients as necessary for the recipe. Using formset required me to add alot to my class based views
+@login_required
+def add_ingredient(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    if request.method == 'POST':
+        form = IngredientForm(request.POST)
+        if form.is_valid():
+            ingredient = form.save(commit=False)
+            ingredient.recipe = recipe
+            ingredient.save()
+    return redirect('recipe-detail', recipe_id=recipe_id)
 
-# I refactored this code again to include a new MixIn, This mixin handles the formset logic. Instead of having this code repeated in all of my classes. I managed to move it all into a mixin and now I call that mixin in my other views to use the logic
-
-
-class RecipeFormWithIngredientsMixin:
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
     form_class = RecipeForm
     template_name = 'recipes/recipe_form.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.request.POST:
-            context['ingredient_formset'] = IngredientFormSet(self.request.POST, instance=getattr(self, 'object', None), prefix='ingredients')
-        else:
-            context['ingredient_formset'] = IngredientFormSet(instance=getattr(self, 'object', None), prefix='ingredients')
-        return context
-
     def form_valid(self, form):
-        context = self.get_context_data()
-        ingredient_formset = context['ingredient_formset']
-
-        if ingredient_formset.is_valid():
-            self.object = form.save(commit=False)
-            self.object.user = self.request.user
-            self.object.save()
-
-            ingredients = ingredient_formset.save(commit=False)
-            for ingredient in ingredients:
-                ingredient.recipe = self.object
-                ingredient.save()
-
-            for ingredient in ingredient_formset.deleted_objects:
-                ingredient.delete()
-
-            return redirect(self.get_success_url())
-        else:
-            return self.render_to_response(self.get_context_data(form=form))
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('recipe-detail', kwargs={'recipe_id': self.object.pk})
+        return reverse('recipe-detail', kwargs={'recipe_id': self.object.pk})
 
-
-class RecipeCreate(LoginRequiredMixin, RecipeFormWithIngredientsMixin, CreateView):
+class RecipeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Recipe
-
-class RecipeUpdate(LoginRequiredMixin, RecipeFormWithIngredientsMixin, UpdateView):
-    model = Recipe
-
-class RecipeDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Recipe
-    template_name = 'recipes/recipe_confirm_delete.html'
-    # After they delete I want the user to be sent to the index page to see the recipe is now deleted.
-    # I added in the UserPassesTestMixin to safeguard for the future implementation of a community page. 
-    success_url = reverse_lazy('recipe-index')
+    form_class = RecipeForm
+    template_name = 'recipes/recipe_form.html'
+    pk_url_kwarg = 'recipe_id'
 
     def test_func(self):
-        # Ensure that only the owner can delete the recipe
         recipe = self.get_object()
         return recipe.user == self.request.user
+
+    def get_success_url(self):
+        return reverse('recipe-detail', kwargs={'recipe_id': self.object.pk})
+
+class RecipeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Recipe
+    template_name = 'recipes/recipe_confirm_delete.html'
+    pk_url_kwarg = 'recipe_id'
+
+    def test_func(self):
+        recipe = self.get_object()
+        return recipe.user == self.request.user
+
+    def get_success_url(self):
+        return reverse('recipe-index')
+
+# Ingredient Views
+
+@login_required
+def ingredient_list(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    ingredients = Ingredient.objects.filter(recipe=recipe)
+    return render(request, 'ingredients/ingredient_list.html', {'recipe': recipe, 'ingredients': ingredients})
+
+@login_required
+def ingredient_create(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    if request.method == 'POST':
+        form = IngredientForm(request.POST)
+        if form.is_valid():
+            ingredient = form.save(commit=False)
+            ingredient.recipe = recipe
+            ingredient.save()
+            return redirect('ingredient-list', recipe_id=recipe_id)
+    else:
+        form = IngredientForm()
+    return render(request, 'ingredients/ingredient_form.html', {'form': form, 'recipe': recipe})
+
+@login_required
+def ingredient_update(request, recipe_id, ingredient_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    ingredient = Ingredient.objects.get(id=ingredient_id, recipe=recipe)
+    if request.method == 'POST':
+        form = IngredientForm(request.POST, instance=ingredient)
+        if form.is_valid():
+            form.save()
+            return redirect('ingredient-list', recipe_id=recipe_id)
+    else:
+        form = IngredientForm(instance=ingredient)
+    return render(request, 'ingredients/ingredient_form.html', {'form': form, 'recipe': recipe})
+
+@login_required
+def ingredient_delete(request, recipe_id, ingredient_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    ingredient = Ingredient.objects.get(id=ingredient_id, recipe=recipe)
+    if request.method == 'POST':
+        ingredient.delete()
+        return redirect('ingredient-list', recipe_id=recipe_id)
+    return render(request, 'ingredients/ingredient_confirm_delete.html', {'ingredient': ingredient, 'recipe': recipe})
